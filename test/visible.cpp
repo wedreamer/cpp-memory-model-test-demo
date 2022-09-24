982ac8f5a8d20c31b7a7c2d36132a7cd4a93f812
shubuzuo <shubuzuo@gmail.com>
2022-09-24 08:58:34 +0800

    fix

--- test/visible.cpp
+++ test/visible.cpp
@@ -7,40 +7,13 @@
 // static 不会导致该问题
 // static bool isStop = false;
 
-static void foo(bool isStop)
-{
-    ASSERT_TRUE(isStop);
-    while (true)
-    {
-        bool b = isStop;
-        if (b)
-        {
-            isStop = false;
-            break;
-        }
-    }
-}
-
-static void func(bool isStop)
-{
-    auto thr = std::thread([&]
-                           {
-                            // 睡 1 ms 
-                            std::this_thread::sleep_for(std::chrono::microseconds(1));
-                            // 停止 foo 的循环
-                            isStop = true; });
-    thr.join();
-    ASSERT_TRUE(isStop);
-    foo(isStop);
-}
-
 // 不会导致永远的循环 -> 还在学习中, java 会导致
 TEST(memory, visible)
 {
     // 停止标记
     // static 不会导致该问题
     // static bool isStop = false;
-    /* bool isStop = false;
+    bool isStop = false;
     auto func = [&]
     {
         auto foo = [&]
@@ -64,11 +37,10 @@ TEST(memory, visible)
                             isStop = true; });
         thr.join();
         foo();
-    }; */
+    };
 
     for (size_t i = 0; i < 100000; i++)
     {
-        bool isStop = false;
-        func(isStop);
+        func();
     }
 }
